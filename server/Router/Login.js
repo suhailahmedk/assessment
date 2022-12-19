5ba02b2fa0b65cb9bb877f5453ccce776e54c473
Suhail Ahmed <suhail.ahmed@deepchain.pk>
2022-12-19 14:11:15 +0500

    update files

--- server/Router/Login.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const roleController=require("../Controller/login.controller");
-const router=express.Router();
-
-router.post("/userLogin",roleController.userLogin);
-
-
-module.exports=router;
